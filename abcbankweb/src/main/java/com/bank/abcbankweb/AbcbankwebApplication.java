297968e8e0b85672251b08dedac1a15262a54afb
Your Name <you@example.com>
2026-02-09 14:01:23 +0530

    abcbank first commit

---
+++ abcbankweb/src/main/java/com/bank/abcbankweb/AbcbankwebApplication.java
@@ -0,0 +1,13 @@
+package com.bank.abcbankweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcbankwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcbankwebApplication.class, args);
+	}
+
+}
---
+++ abcbankweb/src/test/java/com/bank/abcbankweb/AbcbankwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.abcbankweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcbankwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
